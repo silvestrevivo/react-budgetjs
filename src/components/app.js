90535061df01c4859623dcdbaf2e1b090130043e
silvestrevivo <silvestrevivomillan@gmail.com>
2018-04-27 11:04:05 +0200

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => (
+  <div className="container">
+    <img
+      src="https://cdn-images-1.medium.com/max/1600/1*ypTuZbQNEV1oGkAfn85AUA.png"
+      alt="logo-react" />
+    <h1>React Starter</h1>
+  </div>
+)
+
+export default App
